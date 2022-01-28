c073e960e7ad158e0e40827ecccf5e026003d5c9
Luiz Lima <umluizlima@gmail.com>
2022-01-28 20:43:57 -0300

    Add numbers to settings

--- app/settings.py
+++ app/settings.py
@@ -4,6 +4,8 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     TWILIO_ACCOUNT_SID: str
     TWILIO_AUTH_TOKEN: str
+    TWILIO_FROM_NUMBER: str
+    TWILIO_TO_NUMBER: str
 
     class Config:
         env_file = ".env"
