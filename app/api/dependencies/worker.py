1222b9cf3cbbdec8eb83d909070af48d116474bc
Luiz Lima <umluizlima@gmail.com>
2022-02-27 23:04:16 -0300

    Update worker dependency on API

--- app/api/dependencies/worker.py
+++ app/api/dependencies/worker.py
@@ -1,8 +1,8 @@
 from fastapi import Depends
 
 from app.settings import Settings, get_settings
-from app.worker import create_worker
+from app.worker import get_worker
 
 
 def worker(settings: Settings = Depends(get_settings)):
-    return create_worker(settings)
+    return get_worker(settings)
