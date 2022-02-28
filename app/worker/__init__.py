b1bd18ce08578c95dd67467e63f551f733a3bad0
Luiz Lima <umluizlima@gmail.com>
2022-02-27 23:03:56 -0300

    Add get_worker function with lru_cache

--- app/worker/__init__.py
+++ app/worker/__init__.py
@@ -1,9 +1,16 @@
+from functools import lru_cache
+
 from apscheduler.schedulers.background import BackgroundScheduler
 
-from ..settings import Settings
+from ..settings import Settings, get_settings
 
 
 def create_worker(settings: Settings):
     worker = BackgroundScheduler()
 
     return worker
+
+
+@lru_cache(1)
+def get_worker():
+    return create_worker(get_settings())
